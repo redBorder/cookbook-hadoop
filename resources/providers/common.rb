5f9a894e4f3cc43f94c4702b8b5d0e68b0ee70c9
cjmateos <mateos83@gmail.com>
2016-11-02 17:56:53 +0100

    Add hadoop_mapred-site.xml template

--- resources/providers/common.rb
+++ resources/providers/common.rb
@@ -6,33 +6,45 @@
 action :add do #Usually used to install and configure something
   begin
     zookeeper_hosts = new_resource.zookeeper_hosts
+    memory_kb_nodemanager = new_resource.memory_kb_nodemanager
     reservedStackMemory = new_resource.reservedStackMemory
     yarnMemory = new_resource.yarnMemory
     containersMemory = new_resource.containersMemory
     cdomain = node["redborder"]["cdomain"]
 
-     template "/etc/hadoop/yarn-site.xml" do
-         source "hadoop_yarn-site.xml.erb"
-         owner "root"
-         group "root"
-         mode 0644
-         retries 2
-         variables(:zk_hosts => zookeeper_hosts,
-                   :resourcemanager_managers => node["redborder"]["managers_per_services"]["hadoop-resourcemanager"],
-                   :cdomain => cdomain, :yarnMemory => yarnMemory)
-         notifies node["redborder"]["services"]["hadoop-nodemanager"] ? :restart : :nothing, 'service[hadoop-nodemanager]', :delayed
-         notifies node["redborder"]["services"]["hadoop-resourcemanager"] ? :restart : :nothing, 'service[hadoop-resourcemanager]', :delayed
-     end
-
-     template "etc/hadoop/hadoop_yarn-env.sh" do
-        source "hadoop_yarn-env.sh.erb"
+    template "/etc/hadoop/mapred-site.xml" do
+        source "hadoop_mapred-site.xml.erb"
         owner "root"
         group "root"
         mode 0644
         retries 2
-     end
+        variables(:key_id => s3_secrets['key_id'], :key_secret => s3_secrets['key_secret'],
+                  :memory_kb_nodemanager => memory_kb_nodemanager )
+        notifies node["redborder"]["services"]["hadoop-zkfc"] ? :restart : :nothing, 'service[hadoop-zkfc]', :delayed
+    end
 
-     Chef::Log.info("Hadoop cookbook (common) has been processed")
+    template "/etc/hadoop/yarn-site.xml" do
+       source "hadoop_yarn-site.xml.erb"
+       owner "root"
+       group "root"
+       mode 0644
+       retries 2
+       variables(:zk_hosts => zookeeper_hosts,
+                 :resourcemanager_managers => node["redborder"]["managers_per_services"]["hadoop-resourcemanager"],
+                 :cdomain => cdomain, :yarnMemory => yarnMemory)
+       notifies node["redborder"]["services"]["hadoop-nodemanager"] ? :restart : :nothing, 'service[hadoop-nodemanager]', :delayed
+       notifies node["redborder"]["services"]["hadoop-resourcemanager"] ? :restart : :nothing, 'service[hadoop-resourcemanager]', :delayed
+    end
+
+    template "etc/hadoop/hadoop_yarn-env.sh" do
+      source "hadoop_yarn-env.sh.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      retries 2
+    end
+
+    Chef::Log.info("Hadoop cookbook (common) has been processed")
   rescue => e
     Chef::Log.error(e.message)
   end
--- resources/resources/common.rb
+++ resources/resources/common.rb
@@ -7,6 +7,7 @@ actions :add, :remove
 default_action :add
 
 attribute :zookeeper_hosts, :kind_of => String, :default => "zookeeper.service:2181"
+attribute :memory_kb_nodemanager, :kind_of => Integer
 attribute :reservedStackMemory, :kind_of => Integer
 attribute :yarnMemory, :kind_of => Integer
 attribute :containersMemory, :kind_of => Integer
