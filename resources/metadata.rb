0abd409acaa870cb9875e0199e7f1049e37e5fa9
Luis Blanco <ljblanco@redborder.com>
2023-10-24 10:10:29 +0100

    Revert "bugfix/#15368_fix_register_and_deregister"
    This reverts commit 6afca2d488e7d2bce9bd4e9dcc1047624f55e6a9.

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'cjmateos@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures cookbook-hadoop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.0'
--- resources/providers/resourcemanager.rb
+++ resources/providers/resourcemanager.rb
@@ -63,7 +63,7 @@ action :register do
       json_query = Chef::JSONCompat.to_json(query)
 
       execute 'Register service in consul' do
-         command "curl -X PUT http://localhost:8500/v1/agent/service/register -d '#{json_query}' &>/dev/null"
+         command "curl http://localhost:8500/v1/agent/service/register -d '#{json_query}' &>/dev/null"
          action :nothing
       end.run_action(:run)
 
@@ -79,7 +79,7 @@ action :deregister do
   begin
     if node["hadoop"]["resourcemanager"]["registered"]
       execute 'Deregister service in consul' do
-        command "curl -X PUT http://localhost:8500/v1/agent/service/deregister/hadoop-resourcemanager-#{node["hostname"]} &>/dev/null"
+        command "curl http://localhost:8500/v1/agent/service/deregister/hadoop-resourcemanager-#{node["hostname"]} &>/dev/null"
         action :nothing
       end.run_action(:run)
 
